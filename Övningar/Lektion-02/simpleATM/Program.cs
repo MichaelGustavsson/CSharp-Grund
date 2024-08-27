ac9c0a22870086bfdfce182080460fc54b95b780
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-08-27 16:17:37 +0200

    Upated exercises

---
+++ Övningar/Lektion-02/simpleATM/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace simpleATM;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int balance;
+
+        balance = Deposit("Hur mycket vill du sätta in?");
+
+        DisplayBalance(balance);
+
+        var withDraw = WithDraw("Hur mycket vill du ta ut?");
+        balance -= withDraw;
+
+        DisplayBalance(balance);
+    }
+
+    static int Deposit(string message)
+    {
+        Console.WriteLine("Sätt in pengar");
+        var deposit = Console.ReadLine();
+        return int.Parse(deposit!);
+    }
+
+    static int WithDraw(string message)
+    {
+        Console.WriteLine("Hur mycket vill du ta ut?");
+        var withDraw = Console.ReadLine();
+        return int.Parse(withDraw!);
+    }
+
+    static void DisplayBalance(int balance)
+    {
+        Console.WriteLine("Saldo " + balance);
+    }
+}
