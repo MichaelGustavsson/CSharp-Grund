3ceee3af692eae1cd6a7162a18e7183670a74fae
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-09-15 20:23:00 +0200

    Added starter project for lesson 9

---
+++ Lektion-09/InvoiceApp-Starter/Invoice.cs
@@ -0,0 +1,46 @@
+﻿namespace InvoiceApp;
+
+public class Invoice
+{
+  /* PROPERTIES */
+  // Base Info...
+  public int InvoiceNumber { get; }
+  public DateTime InvoiceDate { get; }
+  public DateTime DueDate { get; }
+  public decimal TotalValue { get; private set; }
+  public Customer Customer { get; set; }
+  public Sender Sender { get; set; }
+  /* Invoice Items... */
+  // Aggregation...
+  public List<InvoiceItem> InvoiceItems { get; }
+
+  /* CONSTRUCTORS */
+  // Vi ska ALDRIG kunna skapa en faktura utan kundnummer
+  public Invoice(int customerNumber)
+  {
+    Customer = new Customer(customerNumber);
+    Sender = new Sender();
+    // Generera fakturanummer...
+    InvoiceNumber = new Random().Next(10000, 33000);
+
+    // Skapa fakturadatum...
+    InvoiceDate = DateTime.Now;
+    // Räkna fram förfallodatum...
+    DueDate = InvoiceDate.AddDays(Customer.PaymentTerms);
+
+    // Initiera(skapa) listan av fakturarader...
+    InvoiceItems = new List<InvoiceItem>();
+  }
+
+  /* METHODS */
+  public void AddInvoiceItem(InvoiceItem invoiceItem)
+  {
+    InvoiceItems.Add(invoiceItem);
+    TotalValue += invoiceItem.LineSum;
+  }
+
+  public override string ToString()
+  {
+    return $"{Sender} Fakturanummer: {InvoiceNumber} - Fakturadatum: {InvoiceDate} - Förfallodatum: {DueDate} - {Customer} - TotalValue: {TotalValue}";
+  }
+}
---
+++ Lektion-09/InvoiceApp-Starter/Sender.cs
@@ -0,0 +1,37 @@
+﻿namespace InvoiceApp;
+
+public class Sender
+{
+  /* PROPERTIES */
+  public string Name { get; set; } = "";
+  public string Address { get; set; } = "";
+  public string PostalCode { get; set; } = "";
+  public string City { get; set; } = "";
+  public string Phone { get; set; } = "";
+  public string Reference { get; set; } = "";
+  public string ReferenceEmail { get; set; } = "";
+
+  /* CONSTRUCTORS */
+  public Sender()
+  {
+    LoadSenderInfo();
+  }
+
+  /* METHODS */
+  public override string ToString()
+  {
+    return $"Avsändare: {Name} Address: {Address} {PostalCode} {City} Telefon: {Phone} Vår referens: {Reference}";
+  }
+
+  /* PRIVATE METHODS */
+  private void LoadSenderInfo()
+  {
+    Name = "Vanhedens järnhandel";
+    Address = "Stålgatan 14";
+    PostalCode = "888 98";
+    City = "Hålan";
+    Phone = "073-444222";
+    Reference = "Dynamit Harry";
+    ReferenceEmail = "harry@gmail.com";
+  }
+}
