de51fea23ca5c3f4459628a7af137a9e78c45a98
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-09-17 13:57:59 +0200

    Added project for lesson 10

---
+++ Lektion-10/InvoiceApp-Arv-Step-2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace InvoiceApp;
+
+class Program
+{
+    static void Main()
+    {
+        // Skapa ett objekt ifrån klassen Invoice...
+        // variabeln invoice ska ha en NY(new) kopia(instans)
+        // av klassen Invoice...
+        Invoice invoice = new Invoice(981256);
+        /* FAKTURA RADER */
+        try
+        {
+            invoice.AddInvoiceItem(new InvoiceItem(4, new Product(445869)));
+            invoice.AddInvoiceItem(new InvoiceItem(1, new Product(445878)));
+            // invoice.AddInvoiceItem(new InvoiceItem(1, new Product(445879)));
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Ooops det blev lite fel: {0}", ex.Message);
+        }
+
+
+        // För varje invoiceItem(fakturarad) som finns
+        // i invoice.InvoiceItems(lista av fakturader i objektet)
+        // skriv ut varje fakturarad separat...
+        foreach (var invoiceItem in invoice.InvoiceItems)
+        {
+            Console.WriteLine("Fakturarader: {0}", invoiceItem);
+        }
+
+        Console.WriteLine(invoice);
+
+        // SKAPA EN FEJK PÅMINNELSE...
+        Reminder reminder = new Reminder(123456789);
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("----------------------------------------------------------------");
+        Console.WriteLine("Påminnelse........................");
+        Console.WriteLine("");
+
+        /* FAKTURA RADER */
+        try
+        {
+            reminder.AddInvoiceItem(new InvoiceItem(4, new Product(445869)));
+            reminder.AddInvoiceItem(new InvoiceItem(1, new Product(445878)));
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Ooops det blev lite fel: {0}", ex.Message);
+        }
+        reminder.CalculateReminderTotal();
+        Console.WriteLine(reminder);
+        Console.WriteLine("----------------------------------------------------------------");
+
+
+        // Återställa foreground color...
+        Console.ResetColor();
+
+    }
+}
---
+++ Lektion-10/InvoiceApp-Arv-Step-2/Reminder.cs
@@ -0,0 +1,27 @@
+﻿namespace InvoiceApp;
+
+public class Reminder : Invoice
+{
+  /* PROPERTIES */
+  public decimal Interest { get; set; }
+  public decimal Charge { get; set; }
+
+  /* CONSTRUCTORS */
+  public Reminder(int customerNumber) : base(customerNumber)
+  {
+    Interest = (decimal)0.25;
+  }
+
+  /* METHODS */
+  public void CalculateReminderTotal()
+  {
+    Charge = TotalValue * Interest;
+    Charge += 50;
+    TotalValue += Charge;
+  }
+
+  public override string ToString()
+  {
+    return $"{base.ToString()} Avgift: {Charge}";
+  }
+}
