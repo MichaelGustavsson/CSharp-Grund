5b1c02e6f6937a8c0f708cdb8057a7df8bd567de
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-09-24 16:24:23 +0200

    Added base project Westcoast-shop for lesson 11

---
+++ Lektion-11/Westcoast-shop/Controllers/InvoiceController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Westcoast_shop.Controllers;
+
+public class InvoiceController : Controller
+{
+    // GET: InvoiceController
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+}
+
