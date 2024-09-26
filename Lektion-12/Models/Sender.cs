105ed8855a4f43041516caa7141ba121099884a1
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-09-26 08:47:14 +0200

    Added Models for use in Westcoast E-Shop

---
+++ Lektion-12/Models/Sender.cs
@@ -0,0 +1,24 @@
+﻿namespace InvoiceApp;
+
+public class Sender : Customer
+{
+  /* PROPERTIES */
+  public string ReferenceEmail { get; set; } = "";
+
+  /* CONSTRUCTORS */
+  public Sender()
+  {
+    Find();
+  }
+
+  /* METHODS */
+  public override void Find()
+  {
+    base.Find();
+    ReferenceEmail = "harry@gmail.com";
+  }
+  public override string ToString()
+  {
+    return $"Avsändare: {Name} Address: {Address} Telefon: {Phone} Vår referens: {Reference} e-post: {ReferenceEmail}";
+  }
+}
